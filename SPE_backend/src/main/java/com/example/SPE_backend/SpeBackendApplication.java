5bcbed3bdd1bf95b717cc2b89ea1ea357f527035
Saurabh Varade <saurabhvarade123@gmail.com>
2024-04-23 14:46:51 +0530

    backend setup

---
+++ SPE_backend/src/main/java/com/example/SPE_backend/SpeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SPE_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeBackendApplication.class, args);
+	}
+
+}
---
+++ SPE_backend/src/test/java/com/example/SPE_backend/SpeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SPE_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
