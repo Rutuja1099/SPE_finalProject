e33f30e8b270c5d1788a95e4378d8d0ee0db4194
Saurabh Varade <saurabhvarade123@gmail.com>
2024-04-28 00:47:47 +0530

    tailwind config

--- SPE_final/App.js
+++ SPE_final/App.js
@@ -1,10 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NativeWindStyleSheet } from "nativewind";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import UserInput from "./screens/UserInput";
 
-import UserInput from "./screens/userInput";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -15,15 +13,16 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    
     <NavigationContainer>
 
+     
       <Stack.Navigator initialRouteName="UserInput" >
-
-        {/* <Stack.Screen name="Login" component={Login} options={{headerShown: false,}}/> */}
+      
         <Stack.Screen name="UserInput" component={UserInput} options={{headerShown: false,}}/>
-
+        
       </Stack.Navigator>
     </NavigationContainer>
+  
   );
 }
-
---
+++ SPE_final/screens/userInput.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+const UserInput = () => {
+    return(
+        <SafeAreaView className = "bg-slate-700 mt-10">
+            <Text className = "text-red-700 font-bold text-3xl">hihihiiiiiiiiiiiiiiii</Text>
+        </SafeAreaView>
+    )
+}
+
+export default UserInput;
