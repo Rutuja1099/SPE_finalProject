5f91e2c8a3203712b605dc7a364b5195fd2b4569
Saurabh Varade <saurabhvarade123@gmail.com>
2024-04-29 15:59:48 +0530

    backend folder structure

--- SPE_final/screens/Login.js
+++ SPE_final/screens/Login.js
@@ -1,11 +1,16 @@
 import { Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
+import { ScrollView } from 'react-native-web';
+import { useState } from 'react';
 
 const Login = () => {
 
 
     const navigation = useNavigation();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     const navigateMain = () => {
         navigation.navigate("UserInput");
@@ -14,6 +19,7 @@ const Login = () => {
     return (
         
         <View className = "bg-[#343232] flex-1 justify-center items-center">
+            
             <View className="text-white border-2 border-white rounded-3xl px-8 py-16">
                 <View className="flex items-center mb-8">
                     <Text className="text-white text-3xl">JADE</Text>
@@ -23,12 +29,14 @@ const Login = () => {
                     <TextInput
                         placeholder = "Username"
                         placeholderTextColor="#fff"
-                        className = "border-b-2 border-white pl-2 mb-6 w-56"
+                        className = "border-b-2 border-white pl-2 mb-6 w-56 text-white"
+                        onChangeText={(text) => setUsername(text)}
                     />
                     <TextInput
                         placeholder = "Password"
                         placeholderTextColor="#fff"
-                        className = "border-b-2 border-white pl-2 mb-2"
+                        className = "border-b-2 border-white pl-2 mb-2 text-white"
+                        onChangeText={(text) => setPassword(text)}
                     />
                 </View>
 
@@ -38,8 +46,8 @@ const Login = () => {
                     </Pressable>
                 </View>
 
-                <Pressable className = "flex self-center bg-cyan-500 px-9 py-2 rounded-3xl" onPress={()=>navigateMain()}>
-                    <Text className="text-black text-xl">SUBMIT</Text>
+                <Pressable className = "flex self-center bg-cyan-500 px-9 py-2 rounded-3xl w-48" onPress={()=>navigateMain()}>
+                    <Text className="text-black text-xl self-center">SUBMIT</Text>
                 </Pressable>
                 <Pressable className="flex justify-center items-center mt-2">
                     <Text className="text-stone-400 text-xs">Don't have an account? Sign up</Text>
