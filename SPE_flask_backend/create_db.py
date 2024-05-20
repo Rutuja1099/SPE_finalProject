b71bc7b6f1437a7e382e74aa7220c5c69429e27d
Saurabh Varade <54789865+saurabhv0903@users.noreply.github.com>
2024-05-20 08:43:33 +0530

    Update create_db.py

--- SPE_flask_backend/create_db.py
+++ SPE_flask_backend/create_db.py
@@ -1,16 +1,16 @@
-import mysql.connector
-import pymysql
+# import mysql.connector
+# import pymysql
 
-mydb = pymysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "Saurabh@123",
-    )
+# mydb = pymysql.connect(
+#     host = "localhost",
+#     user = "root",
+#     passwd = "Saurabh@123",
+#     )
 
-my_cursor = mydb.cursor()
+# my_cursor = mydb.cursor()
 
-my_cursor.execute("CREATE DATABASE SPE")
+# my_cursor.execute("CREATE DATABASE SPE")
 
-my_cursor.execute("SHOW DATABASES")
-for db in my_cursor:
-    print(db)
+# my_cursor.execute("SHOW DATABASES")
+# for db in my_cursor:
+#     print(db)
