ee7d305cafe5e2d02ebbad268c4a79a52932d1f5
Saurabh Varade <54789865+saurabhv0903@users.noreply.github.com>
2024-05-20 08:42:14 +0530

    Update app.py

--- SPE_flask_backend/app.py
+++ SPE_flask_backend/app.py
@@ -12,84 +12,84 @@ app = Flask(__name__)
 CORS(app)
  
 # Databse configuration                                  Username:password@hostname/databasename
-password = quote_plus('Saurabh123')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{password}@localhost/SPE'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db=SQLAlchemy(app)
+# password = quote_plus('Saurabh123')
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{password}@localhost/SPE'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db=SQLAlchemy(app)
  
-ma=Marshmallow(app)
+# ma=Marshmallow(app)
  
-class Users(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    date = db.Column(db.DateTime,default=datetime.datetime.now)
+# class Users(db.Model):
+#     __tablename__ = "users"
+#     id = db.Column(db.Integer,primary_key=True)
+#     name = db.Column(db.String(100))
+#     email = db.Column(db.String(100))
+#     date = db.Column(db.DateTime,default=datetime.datetime.now)
  
-    def __init__(self,name,email):
-        self.name=name
-        self.email=email
+#     def __init__(self,name,email):
+#         self.name=name
+#         self.email=email
  
  
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('id','name','email','date')
+# class UserSchema(ma.Schema):
+#     class Meta:
+#         fields = ('id','name','email','date')
  
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+# user_schema = UserSchema()
+# users_schema = UserSchema(many=True)
  
 @app.route('/',methods =['GET'])
 def hello():
     return jsonify("hello")
 
-@app.route('/listusers',methods =['GET'])
-def listusers():
-    all_users = Users.query.all()
-    results = users_schema.dump(all_users)
-    return jsonify(results)
+# @app.route('/listusers',methods =['GET'])
+# def listusers():
+#     all_users = Users.query.all()
+#     results = users_schema.dump(all_users)
+#     return jsonify(results)
  
-@app.route('/userdetails/<id>',methods =['GET'])
-def userdetails(id):
-    user = Users.query.get(id)
-    return user_schema.jsonify(user)
+# @app.route('/userdetails/<id>',methods =['GET'])
+# def userdetails(id):
+#     user = Users.query.get(id)
+#     return user_schema.jsonify(user)
  
-@app.route('/userupdate/<id>',methods = ['PUT'])
-def userupdate(id):
-    user = Users.query.get(id)
+# @app.route('/userupdate/<id>',methods = ['PUT'])
+# def userupdate(id):
+#     user = Users.query.get(id)
  
-    name = request.json['name']
-    email = request.json['email']
+#     name = request.json['name']
+#     email = request.json['email']
  
-    user.name = name
-    user.email = email
+#     user.name = name
+#     user.email = email
  
-    db.session.commit()
-    return user_schema.jsonify(user)
+#     db.session.commit()
+#     return user_schema.jsonify(user)
  
-@app.route('/userdelete/<id>',methods=['DELETE'])
-def userdelete(id):
-    user = Users.query.get(id)
-    db.session.delete(user)
-    db.session.commit()
-    return user_schema.jsonify(user)
+# @app.route('/userdelete/<id>',methods=['DELETE'])
+# def userdelete(id):
+#     user = Users.query.get(id)
+#     db.session.delete(user)
+#     db.session.commit()
+#     return user_schema.jsonify(user)
  
-@app.route('/useradd',methods=['POST'])
-def useradd():
-    name = request.json['name']
-    email = request.json['email']
+# @app.route('/useradd',methods=['POST'])
+# def useradd():
+#     name = request.json['name']
+#     email = request.json['email']
  
-    users = Users(name,email)
-    db.session.add(users)
-    db.session.commit()
-    return user_schema.jsonify(users)
+#     users = Users(name,email)
+#     db.session.add(users)
+#     db.session.commit()
+#     return user_schema.jsonify(users)
  
-@app.route("/image", methods=['GET', 'POST'])
-def image():
-    if(request.method == "POST"):
-        bytesOfImage = request.get_data()
-        with open('image.jpeg', 'wb') as out:
-            out.write(bytesOfImage)
-        return "Image read"
+# @app.route("/image", methods=['GET', 'POST'])
+# def image():
+#     if(request.method == "POST"):
+#         bytesOfImage = request.get_data()
+#         with open('image.jpeg', 'wb') as out:
+#             out.write(bytesOfImage)
+#         return "Image read"
 
 
 @app.route("/video", methods=['POST'])
@@ -100,18 +100,18 @@ def video():
             out.write(bytesOfVideo)
         return "Video read"
 
-def create_tables():
-    db.create_all()
+# def create_tables():
+#     db.create_all()
 
 
-def model():
-    # Load the model
-    model = pickle.load(open('model.pkl','rb'))
+# def model():
+#     # Load the model
+#     model = pickle.load(open('model.pkl','rb'))
 
 
 if __name__=='__main__':
     with app.app_context():
-        create_tables()
+        # create_tables()
         app.run(debug=True)
 
         
