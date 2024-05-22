6623172fe34ce2eb31da8cf91ae98c16251f5bc2
saurabhv0903 <saurabhvarade123@gmail.com>
2024-05-22 23:04:16 +0530

    adding ngrok

--- frontend/configurations/webServer.js
+++ frontend/configurations/webServer.js
@@ -1,4 +1,5 @@
 // const webServerUrl = "http://192.168.234.89:8082";
-const webServerUrl = "http://127.0.0.1:5000";
+// const webServerUrl = "http://127.0.0.1:5000";
+const webServerUrl = "https://civil-basically-bat.ngrok-free.app/";
 
 export default webServerUrl;
