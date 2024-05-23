23e83fc68a0ce264ccf161597a9d0097d8bdd0fe
Saurabh Varade <54789865+saurabhv0903@users.noreply.github.com>
2024-05-23 13:57:25 +0530

    Update webServer.js

--- frontend/configurations/webServer.js
+++ frontend/configurations/webServer.js
@@ -1,5 +1,4 @@
 // const webServerUrl = "http://192.168.234.89:8082";
 // const webServerUrl = "http://127.0.0.1:5000";
-const webServerUrl = "https://civil-basically-bat.ngrok-free.app/";
-
+const webServerUrl = "https://civil-basically-bat.ngrok-free.app";
 export default webServerUrl;
